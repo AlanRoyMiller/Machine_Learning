6bb63f9e16cffa6aa3458c2b877feb3c627a8823
Roy Miller <armiller1612@gmail.com>
2023-03-30 16:56:04 +0200

    Added Normalized_grayscale.py and Pixelate_area.py

---
+++ Pixelate_area.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def prepare_image(
+        image: np.ndarray,
+        x: int,
+        y: int,
+        width: int,
+        height: int,
+        size: int):  # -> tuple[np.ndarray, np.ndarray, np.ndarray]
+
+    pass

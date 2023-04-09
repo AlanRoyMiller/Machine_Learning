085c06c5ff19f80c56ac8b8f295f8168b6309122
Roy Miller <armiller1612@gmail.com>
2023-04-09 16:08:54 +0200

    function now works as expected

--- Normalized _grayscale.py
+++ Normalized _grayscale.py
@@ -3,24 +3,22 @@ import numpy as np
 from PIL import Image
 
 
-
 def to_grayscale(pil_image: np.ndarray) -> np.ndarray:
-    new_image = pil_image / 255.0
-
+    new_image = pil_image / 255.0  # Normalize values
     print(new_image.shape)
 
-    c_linear = np.where((new_image <= 0.04045), (new_image / 12.92), (((new_image + 0.055) / 1.055) ** 2.4))
+    c_linear = np.where((new_image[:, :, :] <= 0.04045), (new_image[:, :, :] / 12.92), (((new_image[:, :, :] + 0.055) / 1.055) ** 2.4))
+    print(new_image.shape)
 
     y_linear = c_linear[:, :, 0] * 0.2126 + c_linear[:, :, 1] * 0.7152 + c_linear[:, :, 2] * 0.0722
+    print(new_image.shape)
 
-    y_srgb = np.where((y_linear <= 0.0031308), (y_linear * 12.92), (((y_linear ** (1 / 2.4)) * 1.055) - 0.055))
+    for i in range(3):
+        c_linear[:, :, i] = y_linear
+
+    y_srgb = np.where((c_linear[:, :, :] <= 0.0031308), (c_linear[:, :, :] * 12.92), (((c_linear[:, :, :] ** (1 / 2.4)) * 1.055) - 0.055))
 
     new_image = y_srgb * 255.0
 
     new_img = Image.fromarray(new_image.astype(np.uint8))
-
     new_img.save('new_image.jpg')
-
-
-    #return ...
-
